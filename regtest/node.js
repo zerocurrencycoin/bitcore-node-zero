b22cea6697359d8028214474c422b7d54a259941
ProphetAlgorithms <38349760+ProphetAlgorithms@users.noreply.github.com>
2018-05-27 16:45:09 +0200

    changes for the Zero node
    enable bitcore-node to work with the Zero node and to create the bitcore-node-zero package

--- regtest/node.js
+++ regtest/node.js
@@ -9,7 +9,7 @@ var log = index.log;
 log.debug = function() {};
 
 var chai = require('chai');
-var bitcore = require('bitcore-lib-zcash');
+var bitcore = require('bitcore-lib-zero');
 var rimraf = require('rimraf');
 var node;
 
