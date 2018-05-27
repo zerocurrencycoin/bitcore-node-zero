06cc5b5fe20d59db76b1a936c353aa81eaefd3b9
ProphetAlgorithms <38349760+ProphetAlgorithms@users.noreply.github.com>
2018-05-27 16:56:54 +0200

    changes for the Zero node
    enable bitcore-node to work with the Zero node and to create the bitcore-node-zero package

--- test/scaffold/default-config.integration.js
+++ test/scaffold/default-config.integration.js
@@ -58,8 +58,8 @@ describe('#defaultConfig', function() {
       services: [
         'bitcoind',
         'web',
-        'insight-api',
-        'insight-ui'
+        'insight-api-zero',
+        'insight-ui-zero'
       ],
       servicesConfig: {
         bitcoind: {
@@ -87,7 +87,7 @@ describe('#defaultConfig', function() {
     });
     var home = process.env.HOME;
     var info = defaultConfig({
-      additionalServices: ['insight-api', 'insight-ui']
+      additionalServices: ['insight-api-zero', 'insight-ui-zero']
     });
     info.path.should.equal(home + '/.bitcore');
     info.config.network.should.equal('livenet');
@@ -95,8 +95,8 @@ describe('#defaultConfig', function() {
     info.config.services.should.deep.equal([
       'bitcoind',
       'web',
-      'insight-api',
-      'insight-ui'
+      'insight-api-zero',
+      'insight-ui-zero'
     ]);
     var bitcoind = info.config.servicesConfig.bitcoind;
     should.exist(bitcoind);
