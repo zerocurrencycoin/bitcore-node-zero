517602c208e70f6bef715b4eb91aa9f2eca1515c
ProphetAlgorithms <38349760+ProphetAlgorithms@users.noreply.github.com>
2018-05-27 16:43:42 +0200

    changes for the Zero node
    enable bitcore-node to work with the Zero node and to create the bitcore-node-zero package

--- regtest/cluster.js
+++ regtest/cluster.js
@@ -6,7 +6,7 @@ var spawn = require('child_process').spawn;
 
 var BitcoinRPC = require('bitcoind-rpc');
 var rimraf = require('rimraf');
-var bitcore = require('bitcore-lib-zcash');
+var bitcore = require('bitcore-lib-zero');
 var chai = require('chai');
 var should = chai.should();
 
@@ -23,7 +23,7 @@ describe('Bitcoin Cluster', function() {
   var nodesConf = [
     {
       datadir: path.resolve(__dirname, './data/node1'),
-      conf: path.resolve(__dirname, './data/node1/zcash.conf'),
+      conf: path.resolve(__dirname, './data/node1/zero.conf'),
       rpcuser: 'bitcoin',
       rpcpassword: 'local321',
       rpcport: 30521,
@@ -32,7 +32,7 @@ describe('Bitcoin Cluster', function() {
     },
     {
       datadir: path.resolve(__dirname, './data/node2'),
-      conf: path.resolve(__dirname, './data/node2/zcash.conf'),
+      conf: path.resolve(__dirname, './data/node2/zero.conf'),
       rpcuser: 'bitcoin',
       rpcpassword: 'local321',
       rpcport: 30522,
@@ -41,7 +41,7 @@ describe('Bitcoin Cluster', function() {
     },
     {
       datadir: path.resolve(__dirname, './data/node3'),
-      conf: path.resolve(__dirname, './data/node3/zcash.conf'),
+      conf: path.resolve(__dirname, './data/node3/zero.conf'),
       rpcuser: 'bitcoin',
       rpcpassword: 'local321',
       rpcport: 30523,
